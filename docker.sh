1669a6bb311caa84860bc0457f33bc78a86a84c6
Philippe Sauter <phsauter@iis.ee.ethz.ch>
2024-08-04 00:35:21 +0800

    docker: fix user id passthrough

--- docker.sh
+++ docker.sh
@@ -10,7 +10,7 @@ IMG=hpretl/iic-osic-tools:pulp
 
 docker pull $IMG
 
-env UID=${UID} GID=${GID} docker-compose run \
+env UID=$(id -u) GID=$(id -g) docker-compose run \
   -e PS1="\[\033[01;32m\]docker-osic: \[\033[00m\]\[\033[01;34m\]\w\[\033[00m\] $" \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
