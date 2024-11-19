ac662df876bcbb043b5239ed829b3f5a7639ef8b
Philippe Sauter <phsauter@iis.ee.ethz.ch>
2024-11-19 21:49:31 +0100

    klayout: add cockpit technology files

--- klayout/def2gds.sh
+++ klayout/def2gds.sh
@@ -2,32 +2,56 @@
 # klayout batch mode for transferring def to gds
 
 # this directory
-script_dir=$(realpath $(dirname "${BASH_SOURCE[0]}"))
-cd $script_dir
+klayout_dir=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+root_dir=$(dirname $klayout_dir)
+cd $klayout_dir
 
 ################
 ### project  ###
 ################
 topcell="croc_chip"
-defpath=$(realpath "$script_dir/../openroad/out/croc.def")
+defpath="$root_dir/openroad/out/croc.def"
 
 
 ################
 ## technology ##
 ################
-pdk=$(realpath "$script_dir/../ihp13/pdk/ihp-sg13g2")
-lef="$(find ${pdk}/libs.ref/sg13g2_stdcell/lef -name '*.lef' -exec realpath {} \;) \
-      $(find ${pdk}/libs.ref/sg13g2_sram/lef -name '*.lef' -exec realpath {} \;) \
-      $(find ${pdk}/libs.ref/sg13g2_io/lef -name 'sg13g2_io.lef' -exec realpath {} \;)"
-gds="$(find ${pdk}/libs.ref/sg13g2_stdcell/gds -name '*.gds' -exec realpath {} \;) \
-      $(find ${pdk}/libs.ref/sg13g2_sram/gds -name '*.gds' -exec realpath {} \;) \
-      $(find ${pdk}/libs.ref/sg13g2_io/gds -name '*.gds' -exec realpath {} \;)"
-tech="$pdk/libs.tech/klayout/tech/sg13g2.lyt"
-layer="$pdk/libs.tech/klayout/tech/sg13g2.lyp"
+if [[ -f "$root_dir/.cockpitrc" ]]; then
+    echo "Init tech from ETHZ DZ cockpit"
+    pdk_dir=$(realpath "$root_dir/technology")
+    pdk_cells_lef_dir="${pdk_dir}/lef"
+    pdk_sram_lef_dir="${pdk_dir}/lef"
+    pdk_io_lef_dir="${pdk_dir}/lef"
+    pdk_cells_gds_dir="${pdk_dir}/gds"
+    pdk_sram_gds_dir="${pdk_dir}/gds"
+    pdk_io_gds_dir="${pdk_dir}/gds"
+else
+    echo "Init tech from Github PDK"
+    pdk_dir=${pdk_dir:-$(realpath "$root_dir/ihp13/pdk")}
+    pdk="${pdk_dir}/ihp-sg13g2"
+    pdk_cells_lef_dir="${pdk}/libs.ref/sg13g2_stdcell/lef"
+    pdk_sram_lef_dir="${pdk}/libs.ref/sg13g2_sram/lef"
+    pdk_io_lef_dir="${pdk}/libs.ref/sg13g2_io/lef"
+    pdk_cells_gds_dir="${pdk}/libs.ref/sg13g2_stdcell/gds"
+    pdk_sram_gds_dir="${pdk}/libs.ref/sg13g2_sram/gds"
+    pdk_io_gds_dir="${pdk}/libs.ref/sg13g2_io/gds"
+fi
+
+lef="$(find "$pdk_cells_lef_dir" -name 'sg13g2_stdcell.lef' -exec realpath {} \;) \
+     $(find "$pdk_cells_lef_dir" -name 'sg13g2_tech.lef' -exec realpath {} \;) \
+     $(find "$pdk_sram_lef_dir" -name 'RM_IHPSG13*.lef' -exec realpath {} \;) \
+     $(find "$pdk_io_lef_dir" -name 'sg13g2_io.lef' -exec realpath {} \;)"
+
+gds="$(find "$pdk_cells_gds_dir" -name 'sg13g2_stdcell.gds' -exec realpath {} \;) \
+     $(find "$pdk_sram_gds_dir" -name 'RM_IHPSG13*.gds' -exec realpath {} \;) \
+     $(find "$pdk_io_gds_dir" -name 'sg13g2_io.gds' -exec realpath {} \;)"
+
+tech="$root_dir/ihp13/pdk/ihp-sg13g2/libs.tech/klayout/tech/sg13g2.lyt"
+layer="$root_dir/ihp13/pdk/ihp-sg13g2/libs.tech/klayout/tech/sg13g2.lyp"
 
 # create klayout home dir and add pdk to path
-export KLAYOUT_HOME="$script_dir/.klayout"
-export KLAYOUT_PATH="$(realpath $script_dir/../ihp13/pdk/ihp-sg13g2/libs.tech/klayout):$KLAYOUT_PATH"
+export KLAYOUT_HOME="$klayout_dir/.klayout"
+export KLAYOUT_PATH="$(realpath $root_dir/ihp13/pdk/ihp-sg13g2/libs.tech/klayout):$KLAYOUT_PATH"
 mkdir -p $KLAYOUT_HOME/tech
 
 # all <lef-files> entries for the tech file
@@ -37,15 +61,15 @@ for lef_file in $lef; do
 done
 
 # replace the placeholder tag with the real lef files
-sed "/<lef-files>merged.lef<\/lef-files>/c $lef_files" "$tech" > $KLAYOUT_HOME/tech/sg13g2.lyt
-# use the lef/def map file
-sed -i "s/<map-file\/>/<map-file>sg13g2.map<\/map-file>/g" $KLAYOUT_HOME/tech/sg13g2.lyt
-ln -sfr $script_dir/sg13g2.map $KLAYOUT_HOME/tech/sg13g2.map
+sed "/<lef-files><\/lef-files>/c $lef_files" "$tech" > $KLAYOUT_HOME/tech/sg13g2.lyt
+ln -sfr $klayout_dir/sg13g2.map $KLAYOUT_HOME/tech/sg13g2.map
 
-klayout_cmd="/foss/tools/klayout/klayout -zz \
+echo "$gds" > $KLAYOUT_HOME/tech/tech_gds.f
+
+klayout_cmd="$KLAYOUT -zz \
           -rd design_name=\"$topcell\" \
           -rd in_def=\"$defpath\" \
-          -rd in_files=\"$gds\" \
+          -rd gds_flist=\"$KLAYOUT_HOME/tech/tech_gds.f\" \
           -rd out_file=\"${topcell}.gds\" \
           -rd tech_file=\"$KLAYOUT_HOME/tech/sg13g2.lyt\" \
           -rd layer_map=\"$KLAYOUT_HOME/tech/sg13g2.map\" \
