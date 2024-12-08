31d4e3139948e050430e7ce1e92ed6feb9b1275f
luisawuethrich <lwuethri@ethz.ch>
2024-12-08 14:48:23 +0100

    fix: neopixelcontroller naming and fpga neopixel commands: dog animation

--- rtl/neopixel/neopixel_controller.sv
+++ rtl/neopixel/neopixel_controller.sv
@@ -86,7 +86,7 @@ module neopixel_controller import neopixel_pkg::*; #() (
     // The data is sent in GRB order
     logic [23:0] active_color_data_q, active_color_data_d;
 
-    logic last_state_idle_d, last_state_idle_q;
+    logic last_state_latch_d, last_state_latch_q;
 
     // Indexcounter for the active_color_data array
     logic [$clog2(24) - 1:0] active_color_data_index_q, active_color_data_index_d;
@@ -97,7 +97,7 @@ module neopixel_controller import neopixel_pkg::*; #() (
     // Next state and output logic
     always_comb begin
         state_d = state_q;
-        last_state_idle_d = last_state_idle_q;
+        last_state_latch_d = last_state_latch_q;
         data_o = 1'b0;
         counter_clear = 1'b0;
         counter_en = 1'b0;
@@ -113,11 +113,11 @@ module neopixel_controller import neopixel_pkg::*; #() (
                 neopixel_index_d = 0;
                 active_color_data_index_d = 0;
 
-                if (last_state_idle_q & sleep != 0) begin
+                if (last_state_latch_q & sleep != 0) begin
                     counter_en = 1'b1;
-                    last_state_idle_d = (counter_q == sleep)? 1'b0: last_state_idle_q;
+                    last_state_latch_d = (counter_q == sleep)? 1'b0: last_state_latch_q;
                 end else begin
-                    last_state_idle_d = 1'b0;
+                    last_state_latch_d = 1'b0;
                     if (~fifo_empty_i & num_neopixel > 0) begin
                         // Start transmission if FIFO has data and NeoPixels are defined
                         state_d = BIT_HIGH;
@@ -174,7 +174,7 @@ module neopixel_controller import neopixel_pkg::*; #() (
                 if (counter_q == t_latch - 1) begin
                         state_d = IDLE;
                         counter_clear = 1'b1;
-                        last_state_idle_d = 1'b1;
+                        last_state_latch_d = 1'b1;
                 end
             end
 
@@ -190,6 +190,6 @@ module neopixel_controller import neopixel_pkg::*; #() (
     `FF(active_color_data_q, active_color_data_d, '0, clk_i, rst_ni)
     `FF(active_color_data_index_q, active_color_data_index_d, '0, clk_i, rst_ni)
     `FF(neopixel_index_q, neopixel_index_d, '0, clk_i, rst_ni)
-    `FF(last_state_idle_q, last_state_idle_d, '0, clk_i, rst_ni)
+    `FF(last_state_latch_q, last_state_latch_d, '0, clk_i, rst_ni)
 
 endmodule
