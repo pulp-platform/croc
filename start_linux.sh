49b929bfb35bae6a6bfed9bd8b345d179bcf37b0
Philippe Sauter <phsauter@iis.ee.ethz.ch>
2025-02-18 15:22:50 +0100

    tools: change docker-compose to docker compose

--- start_linux.sh
+++ start_linux.sh
@@ -10,8 +10,7 @@ IMG=hpretl/iic-osic-tools:2024.10
 
 docker pull $IMG
 
-env UID=$(id -u) GID=$(id -g) docker-compose run \
+env UID=$(id -u) GID=$(id -g) docker compose run \
   -e PS1="\[\033[01;32m\]docker-osic: \[\033[00m\]\[\033[01;34m\]\w\[\033[00m\] $" \
-  -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   pulp-docker
