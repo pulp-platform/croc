7a7ec0849f3767acee7209effbe6e3cd8a1d1a3e
micha4w <micha4w@gmail.com>
2025-03-21 09:39:24 +0100

    docker: add display variable

--- start_linux.sh
+++ start_linux.sh
@@ -12,5 +12,6 @@ docker pull $IMG
 
 env UID=$(id -u) GID=$(id -g) docker compose run \
   -e PS1="\[\033[01;32m\]docker-osic: \[\033[00m\]\[\033[01;34m\]\w\[\033[00m\] $" \
+  -e DISPLAY=:1 \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   pulp-docker
