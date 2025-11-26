97314af155cb32625a368a3ceb9fc1af6f6347cc
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2025-11-26 01:33:17 +0100

    rtl: add OBI timer

---
+++ rtl/obi_timer/obi_timer_pkg.sv
@@ -0,0 +1,12 @@
+package obi_timer_pkg;
+
+  // Internal address width. We only need 4 bits to
+  // store the offsets of the three registers implemented.
+  localparam int unsigned IntAddrWidth = 5;
+
+  // Register offsets
+  parameter logic [IntAddrWidth-1:0] OBI_TIMER_COUNT_OFFSET    = 4'h0;
+  parameter logic [IntAddrWidth-1:0] OBI_TIMER_COMPARE_OFFSET  = 4'h4;
+  parameter logic [IntAddrWidth-1:0] OBI_TIMER_ENABLE_OFFSET   = 4'h8;
+
+endpackage
