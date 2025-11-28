9bbfb0dc66c8be0508e14768e0b6dfc18a6459a4
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2025-11-28 09:22:28 +0100

    openroad: add run.sh bash script

---
+++ openroad/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set variables
+OPENROAD=${OPENROAD:-openroad}
+QT_QPA_PLATFORM=${QT_QPA_PLATFORM:-"offscreen"}
+
+# Clean up generated files
+[ -f croc.log ] && rm croc.log
+rm -rf reports/
+rm -rf out/
+rm -rf save/
+mkdir reports
+mkdir out
+mkdir save
+
+# Run OpenRoad
+QT_QPA_PLATFORM="offscreen" ${OPENROAD} scripts/chip.tcl -log croc.log 2>&1 | TZ=UTC gawk '{ print strftime("[%Y-%m-%d %H:%M %Z]"), $0 }'
+
