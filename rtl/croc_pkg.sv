0dfc2005596ad2ce58092cd80e41249a5fcfdaf6
phsauter <phsauter@student.ethz.ch>
2025-12-12 01:07:51 +0100

    rtl: simplify configuration in croc_pkg

--- rtl/croc_pkg.sv
+++ rtl/croc_pkg.sv
@@ -5,46 +5,48 @@
 // Authors:
 // - Philippe Sauter <phsauter@iis.ee.ethz.ch>
 
+// header files for the two interconnect types used in Croc
 `include "register_interface/typedef.svh"
 `include "obi/typedef.svh"
 
+/// Package for croc configuration, address map and interconnect types
 package croc_pkg;
-
-  localparam int unsigned HartId = 32'd0;
-
-    // Default JTAG ID code type
-  typedef struct packed {
+  //////////////////////
+  // JTAG Info        //
+  //////////////////////
+  /// JTAG IDCODE for the Croc JTAG tap, used to identify the device
+  localparam struct packed {
     bit [ 3:0]  version;
     bit [15:0]  part_num;
     bit [10:0]  manufacturer;
     bit         _one;
-  } jtag_idcode_t;
-
-  // PULP Platform manufacturer and default Cheshire part number
-  localparam bit [10:0] JtagPulpManufacturer = 11'h6d9;
-  localparam bit [15:0] JtagCrocPartNum      = 16'hC0C5;
-  localparam bit [ 3:0] JtagCrocVersion      = 4'h0;
-  localparam jtag_idcode_t PulpJtagIdCode = '{
-    _one          : 1,
-    manufacturer  : JtagPulpManufacturer,
-    part_num      : JtagCrocPartNum,
-    version       : JtagCrocVersion
+  } PulpJtagIdCode = '{
+    _one:         1'b1,     /* must be 1 */
+    manufacturer: 11'h6d9,  /* identify as PULP Platform chip */
+    part_num:     16'hC0C5, /* default Croc part number */
+    version:      4'h0      /* version 0 */
   };
 
-  typedef enum logic {
-    Jtag = 1'b0
-  } bootmode_e;
 
-  // Number of additional interrupts coming into croc_domain and going to the core
-  localparam int unsigned NumExternalIrqs = 4;
+  ////////////////////////
+  // SRAM Configuration //
+  ////////////////////////
+  // Check your target technology which SRAMs are available
+  // and then make sure it is implemented as an option in tc_sram_impl.sv
+  
+  /// Number of SRAM banks, each bank has its own OBI port (accessible in parallel)
+  localparam int unsigned NumSramBanks      = 32'd2;
+  /// Number of 32-bit words per SRAM bank, determines the depth of each SRAM bank
+  localparam int unsigned SramBankNumWords  = 512;
 
 
-  ///////////////////////
-  // Address Maps     ///
-  ///////////////////////
-  // ideally compatible with: https://pulp-platform.github.io/cheshire/um/arch/#memory-map
+  //////////////////////
+  // Address Map Type //
+  //////////////////////
+  // ideally compatible with:
+  // https://pulp-platform.github.io/cheshire/um/arch/#memory-map
 
-  // Address map data type
+  /// Address map data type
   typedef struct packed {
       logic [31:0] idx;
       logic [31:0] start_addr;
@@ -52,81 +54,54 @@ package croc_pkg;
   } addr_map_rule_t;
 
 
-  ////////////////////////////////////////
-  // Croc Main interconnect addressing ///
-  ////////////////////////////////////////
+  ////////////////////////////////
+  // Main Crossbar Address Map ///
+  ////////////////////////////////
+  /// Number of manager ports into crossbar
+  /// Debug module, Core Instr, Core Data, User Domain
+  localparam int unsigned NumXbarManagers = 4;
 
-  localparam bit [31:0]   PeriphBaseAddr    = 32'h0000_0000;
-  localparam bit [31:0]   PeriphAddrRange   = 32'h1000_0000;
-
-  localparam bit [31:0]   SramBaseAddr      = 32'h1000_0000;
-  localparam int unsigned NumSramBanks      = 32'd2;
-  localparam int unsigned SramBankNumWords  = 512;
-  localparam int unsigned SramBankAddrWidth = cf_math_pkg::idx_width(SramBankNumWords);
-  localparam int unsigned SramAddrRange     = NumSramBanks*SramBankNumWords*4;
-
-  localparam bit [31:0]   UserBaseAddr      = 32'h2000_0000;
-  localparam bit [31:0]   UserAddrRange     = 32'h6000_0000;
-
-  localparam int unsigned NumCrocDomainSubordinates = 2 + NumSramBanks; // Peripherals + Memory + User Domain
-  
-  localparam int unsigned NumXbarManagers = 4; // Debug module, Core Instr, Core Data, User Domain
-  localparam int unsigned NumXbarSbrRules = NumCrocDomainSubordinates; // number of address rules in the decoder
-  localparam int unsigned NumXbarSbr      = NumXbarSbrRules + 1; // additional OBI error, used for signal arrays
-
-  // Enum for bus indices
+  /// Enum with crossbar subordinate idxs
   typedef enum int {
     XbarError  = 0,
     XbarPeriph = 1,
-    XbarBank0  = 2,
-    XbarUser   = 2 + NumSramBanks
+    XbarUser   = 2,
+    XbarBank0  = 3
   } croc_xbar_outputs_e;
 
-  // generate the address rules dependent on the number of SRAM banks
-  function automatic addr_map_rule_t [NumXbarSbrRules-1:0] gen_xbar_addr_rules();
-    addr_map_rule_t [NumXbarSbrRules-1:0] ret;
-    ret[0] = '{ idx: XbarPeriph,
-                start_addr: PeriphBaseAddr,
-                end_addr:   PeriphBaseAddr+PeriphAddrRange};
+  /// Address map given to the main crossbar
+  localparam addr_map_rule_t [3:0] croc_addr_map = '{
+    '{ idx: XbarPeriph,  start_addr: 32'h0000_0000, end_addr: 32'h1000_0000 },
+    '{ idx: XbarUser,    start_addr: 32'h2000_0000, end_addr: 32'h8000_0000 },
+    '{ idx: XbarBank0,   start_addr: 32'h1000_0000, end_addr: 32'h1000_0800 },
+    '{ idx: XbarBank0+1, start_addr: 32'h1000_0800, end_addr: 32'h1000_1000 }
+  };
 
-    for (int i = 0; i < NumSramBanks; i++) begin
-      ret[i+1] = '{ idx: XbarBank0+i,
-                    start_addr: SramBaseAddr + ( i    * SramBankNumWords*4),
-                    end_addr:   SramBaseAddr + ((i+1) * SramBankNumWords*4)};
+  // +1 for additional OBI error
+  localparam int unsigned NumXbarSubordinates = $size(croc_addr_map) + 1;
 
-      ret[NumXbarSbrRules-1] = '{ idx: XbarUser,
-                start_addr: UserBaseAddr,
-                end_addr:   UserBaseAddr+UserAddrRange};
+  /// Converts the bus idx enum for the main crossbar to start address of a subordinate
+  /// Eg : get_croc_start_addr(XbarUser) returns the start address of the User region
+  /// This is necesary because the idx do not directly correspond to the indices of the array
+  function automatic bit [31:0] get_croc_start_addr(croc_xbar_outputs_e port);
+    bit [31:0] addr = '0;
+    for (int unsigned i = 0; i < $size(croc_addr_map); i++) begin
+      if (croc_xbar_outputs_e'(croc_addr_map[i].idx) == port) begin
+        return croc_addr_map[i].start_addr;
+      end
     end
-    return ret;
+    return addr;
   endfunction
 
-  localparam addr_map_rule_t [NumXbarSbrRules-1:0] croc_addr_map = gen_xbar_addr_rules();
+  // For convenience get most used base addresses
+  localparam bit [31:0] UserBaseAddr = get_croc_start_addr(XbarUser); // use as base for your IPs
+  localparam bit [31:0] BootAddr     = get_croc_start_addr(XbarBank0); // start of SRAM banks
 
 
-  /////////////////////////////
-  // Peripheral address map ///
-  /////////////////////////////
-
-  localparam bit [31:0] DebugAddrOffset   = 32'h0000_0000;
-  localparam bit [31:0] DebugAddrRange    = 32'h0004_0000;
-
-  localparam bit [31:0] SocCtrlAddrOffset = 32'h0300_0000;
-  localparam bit [31:0] SocCtrlAddrRange  = 32'h0000_1000;
-
-  localparam bit [31:0] UartAddrOffset    = 32'h0300_2000;
-  localparam bit [31:0] UartAddrRange     = 32'h0000_1000;
-
-  localparam bit [31:0] GpioAddrOffset    = 32'h0300_5000;
-  localparam bit [31:0] GpioAddrRange     = 32'h0000_1000;
-
-  localparam bit [31:0] TimerAddrOffset   = 32'h0300_A000;
-  localparam bit [31:0] TimerAddrRange    = 32'h0000_1000;
-
-  localparam int unsigned NumPeriphRules  = 5;
-  localparam int unsigned NumPeriphs      = NumPeriphRules + 1; // additional OBI error
-
-  // Enum for bus indices
+  ////////////////////////////////
+  // Peripheral Mux Address Map //
+  ////////////////////////////////
+  /// Enum with peripheral mux subordinate idxs
   typedef enum int {
     PeriphError    = 0,
     PeriphDebug    = 1,
@@ -136,35 +111,54 @@ package croc_pkg;
     PeriphTimer    = 5
   } periph_outputs_e;
 
-  localparam addr_map_rule_t [NumPeriphRules-1:0] periph_addr_map = '{                                       // 0: OBI Error (default)
-    '{ idx: PeriphDebug,    start_addr: DebugAddrOffset,    end_addr: DebugAddrOffset   + DebugAddrRange},   // 1: Debug
-    '{ idx: PeriphSocCtrl,  start_addr: SocCtrlAddrOffset,  end_addr: SocCtrlAddrOffset + SocCtrlAddrRange}, // 2: SoC control
-    '{ idx: PeriphUart,     start_addr: UartAddrOffset,     end_addr: UartAddrOffset    + UartAddrRange},    // 3: UART
-    '{ idx: PeriphGpio,     start_addr: GpioAddrOffset,     end_addr: GpioAddrOffset    + GpioAddrRange},    // 4: GPIO
-    '{ idx: PeriphTimer,    start_addr: TimerAddrOffset,    end_addr: TimerAddrOffset   + TimerAddrRange}    // 5: Timer
+  /// Address map given to the peripheral mux
+  localparam addr_map_rule_t [4:0] periph_addr_map = '{
+    '{ idx: PeriphDebug,   start_addr: 32'h0000_0000, end_addr: 32'h0004_0000 },
+    '{ idx: PeriphSocCtrl, start_addr: 32'h0300_0000, end_addr: 32'h0300_1000 },
+    '{ idx: PeriphUart,    start_addr: 32'h0300_2000, end_addr: 32'h0300_3000 },
+    '{ idx: PeriphGpio,    start_addr: 32'h0300_5000, end_addr: 32'h0300_6000 },
+    '{ idx: PeriphTimer,   start_addr: 32'h0300_A000, end_addr: 32'h0300_B000 }
   };
 
+  // +1 for additional OBI error
+  localparam int unsigned NumPeriphs = $size(periph_addr_map) + 1;
+
+  /// Converts the bus indices enum for the peripheral mux to start address of a peripheral
+  /// Eg : get_periph_start_addr(PeriphGpio) returns the start address of the GPIO peripheral
+  /// This is necesary because the idx do not directly correspond to the indices of the array
+  function automatic bit [31:0] get_periph_start_addr(periph_outputs_e port);
+    bit [31:0] addr = '0;
+    for (int unsigned i = 0; i < $size(periph_addr_map); i++) begin
+      if (periph_outputs_e'(periph_addr_map[i].idx) == port) begin
+        return periph_addr_map[i].start_addr;
+      end
+    end
+    return addr;
+  endfunction
+
+
+  ///////////////////////////////////////////
+  // Interconnect Types and Configurations //
+  ///////////////////////////////////////////
   // OBI is configured as 32 bit data, 32 bit address width
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
- 
   // Usually we would use the typedef.svh macros to define interconnects from parameters.
   // To make it easier to understand we instead write them out here,
-  // at the end of the block in a comment you can find the equivalent using the macros.
+  // in a comment at the end you can find the equivalent using the macros.
 
-  /// OBI managers configuration (from a manager into the interconnect)
+  /// OBI manager configuration (from a manager into the crossbar)
   localparam obi_pkg::obi_cfg_t MgrObiCfg = '{
-                                  UseRReady:   1'b0,
-                                  CombGnt:     1'b0,
-                                  AddrWidth:     32,
-                                  DataWidth:     32,
-                                  IdWidth:        1,
-                                  Integrity:   1'b0,
-                                  BeFull:      1'b1,
-                                  OptionalCfg:  '0
-                                };
-  /// OBI Manager <-> Xbar address channel
+    UseRReady:   1'b0,
+    CombGnt:     1'b0,
+    AddrWidth:     32,
+    DataWidth:     32,
+    IdWidth:        1,
+    Integrity:   1'b0,
+    BeFull:      1'b1,
+    OptionalCfg:  '0
+  };
+
+  /// OBI Manager -> Xbar address channel
   typedef struct packed {
     logic [  MgrObiCfg.AddrWidth-1:0] addr;
     logic                             we;
@@ -173,37 +167,38 @@ package croc_pkg;
     logic [    MgrObiCfg.IdWidth-1:0] aid;
     logic                             a_optional; // dummy signal; not used
   } mgr_obi_a_chan_t;
-  /// OBI Manager <-> Xbar request
+  /// OBI Manager -> Xbar request
   typedef struct packed {
     mgr_obi_a_chan_t a;
     logic            req;
   } mgr_obi_req_t;
-  /// OBI Manager <-> Xbar response channel
+
+  /// OBI Manager <- Xbar response channel
   typedef struct packed {
     logic [MgrObiCfg.DataWidth-1:0] rdata;
     logic [  MgrObiCfg.IdWidth-1:0] rid;
     logic                           err;
     logic                           r_optional; // dummy signal; not used
   } mgr_obi_r_chan_t;
-  /// OBI Manager <-> Xbar response
+  /// OBI Manager <- Xbar response
   typedef struct packed {
     mgr_obi_r_chan_t r;
     logic            gnt;
     logic            rvalid;
   } mgr_obi_rsp_t;
 
-  /// OBI subordinate configuration (from the interconnect to a subordinate device)
+  /// OBI subordinate configuration (from the crossbar to a subordinate device)
   localparam obi_pkg::obi_cfg_t SbrObiCfg = '{
-                                  UseRReady:   1'b0,
-                                  CombGnt:     1'b0,
-                                  AddrWidth:     32,
-                                  DataWidth:     32,
-                                  IdWidth:        1 + cf_math_pkg::idx_width(NumXbarManagers),
-                                  Integrity:   1'b0,
-                                  BeFull:      1'b1,
-                                  OptionalCfg:  '0
-                                };
-  /// OBI Xbar <-> Subordinate address channel
+    UseRReady:   1'b0,
+    CombGnt:     1'b0,
+    AddrWidth:     32,
+    DataWidth:     32,
+    IdWidth:        1 + cf_math_pkg::idx_width(NumXbarManagers),
+    Integrity:   1'b0,
+    BeFull:      1'b1,
+    OptionalCfg:  '0
+  };
+  /// OBI Xbar -> Subordinate address channel
   typedef struct packed {
     logic [  SbrObiCfg.AddrWidth-1:0] addr;
     logic                             we;
@@ -212,40 +207,25 @@ package croc_pkg;
     logic [    SbrObiCfg.IdWidth-1:0] aid;
     logic                             a_optional; // dummy signal; not used
   } sbr_obi_a_chan_t;
-  /// OBI Xbar <-> Subordinate request
+  /// OBI Xbar -> Subordinate request
   typedef struct packed {
     sbr_obi_a_chan_t a;
     logic            req;
   } sbr_obi_req_t;
-  /// OBI Xbar <-> Subordinate response channel
+
+  /// OBI Xbar <- Subordinate response channel
   typedef struct packed {
     logic [SbrObiCfg.DataWidth-1:0] rdata;
     logic [  SbrObiCfg.IdWidth-1:0] rid;
     logic                           err;
     logic                           r_optional; // dummy signal; not used
   } sbr_obi_r_chan_t;
-  /// OBI Xbar <-> Subordinate response
+  /// OBI Xbar <- Subordinate response
   typedef struct packed {
     sbr_obi_r_chan_t r;
     logic            gnt;
     logic            rvalid;
   } sbr_obi_rsp_t;
-
-  // Register interface (regbus) request
-  typedef struct packed {
-      logic [31:0] addr;
-      logic        write;
-      logic [31:0] wdata;
-      logic [ 3:0] wstrb;
-      logic        valid;
-  } reg_req_t;
-
-  // Register interface (regbus) response
-  typedef struct packed {
-      logic [31:0] rdata;
-      logic        error;
-      logic        ready;
-  } reg_rsp_t;
 
   /* This is how we would usually define interconnects using the typedef.svh macros
    *
@@ -264,6 +244,5 @@ package croc_pkg;
    * // Register Interface configured as 32 bit data, 32 bit address width (4 byte enable bits)
    * `REG_BUS_TYPEDEF_ALL(reg, logic[31:0], logic[31:0], logic[3:0]); 
    */
-
 
 endpackage
