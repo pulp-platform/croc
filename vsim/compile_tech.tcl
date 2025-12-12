6aeed6db2bf9a82ce711b40d9c5bbb1d8858fb7b
Philippe Sauter <phsauter@iis.ee.ethz.ch>
2025-12-12 12:32:11 +0100

    vsim: add Siemens software program to header

--- vsim/compile_tech.tcl
+++ vsim/compile_tech.tcl
@@ -2,6 +2,10 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 #
+# Published with permission from Siemens. 
+# Siemens QuestaSim is available through EDA Higher Education Software Program
+# https://www.sw.siemens.com/en-US/academic/educators/eda-higher-education-software/
+#
 # Authors:
 # - Philippe Sauter <phsauter@iis.ee.ethz.ch>
 
