7fc7efb77f71f4199bee0e10865b9734324f6d47
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2025-12-15 00:49:31 +0100

    hw: prevent negative index in user_pkg

--- rtl/user_pkg.sv
+++ rtl/user_pkg.sv
@@ -25,7 +25,7 @@ package user_pkg;
   localparam bit [31:0] UserRomAddrOffset   = croc_pkg::UserBaseAddr; // 32'h2000_0000;
   localparam bit [31:0] UserRomAddrRange    = 32'h0000_1000;          // every subordinate has at least 4KB
 
-  localparam int unsigned NumDemuxSbrRules  = NumUserDomainSubordinates; // number of address rules in the decoder
+  localparam int unsigned NumDemuxSbrRules  = (NumUserDomainSubordinates > 0) ? NumUserDomainSubordinates : 1; // number of address rules in the decoder
   localparam int unsigned NumDemuxSbr       = NumDemuxSbrRules + 1; // additional OBI error, used for signal arrays
 
   // Enum for bus indices
