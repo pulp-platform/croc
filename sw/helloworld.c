9c92d3b780d7fdd56a9f02611f6ed5ebc0c4e4b8
phsauter <phsauter@student.ethz.ch>
2026-02-21 03:06:20 +0100

    sw: simplify helloworld to minimum

--- sw/helloworld.c
+++ sw/helloworld.c
@@ -1,86 +1,17 @@
 // Copyright (c) 2024 ETH Zurich and University of Bologna.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
-// SPDX-License-Identifier: Apache-2.0/
+// SPDX-License-Identifier: Apache-2.0
 //
 // Authors:
 // - Philippe Sauter <phsauter@iis.ee.ethz.ch>
 
 #include "uart.h"
 #include "print.h"
-#include "gpio.h"
-#include "clint.h"
-#include "util.h"
-
-/// @brief Example integer square root
-/// @return integer square root of n
-uint32_t isqrt(uint32_t n) {
-    uint32_t res = 0;
-    uint32_t bit = (uint32_t)1 << 30;
-
-    while (bit > n) bit >>= 2;
-
-    while (bit) {
-        if (n >= res + bit) {
-            n -= res + bit;
-            res = (res >> 1) + bit;
-        } else {
-            res >>= 1;
-        }
-        bit >>= 2;
-    }
-    return res;
-}
-
-char receive_buff[16] = {0};
+#include "config.h"
 
 int main() {
-    uart_init(); // setup the uart peripheral
-
-    // simple printf support (only prints text and hex numbers)
-    printf("Hello World!\n");
-    // wait until uart has finished sending
-    uart_write_flush();
-
-    // uart loopback
-    uart_loopback_enable();
-    printf("internal msg\n");
-    clint_sleep_ms(1);
-    for (uint8_t idx = 0; idx < 15; idx++) {
-        receive_buff[idx] = uart_read();
-        if (receive_buff[idx] == '\n') {
-            break;
-        }
-    }
-    uart_loopback_disable();
-
-    printf("Loopback received: ");
-    printf(receive_buff);
-    uart_write_flush();
-
-    // toggling some GPIOs
-    gpio_set_direction(0xFFFF, 0x000F); // lowest four as outputs
-    gpio_write(0x0A);                   // ready output pattern
-    gpio_enable(0xFF);                  // enable lowest eight
-    // wait a few cycles to give GPIO signal time to propagate
-    asm volatile("nop; nop; nop; nop; nop;");
-    printf("GPIO (expect 0xA0): 0x%x\n", gpio_read());
-
-    gpio_toggle(0x0F); // toggle lower 8 GPIOs
-    asm volatile("nop; nop; nop; nop; nop;");
-    printf("GPIO (expect 0x50): 0x%x\n", gpio_read());
-    uart_write_flush();
-
-    // doing some compute
-    uint32_t start = get_mcycle();
-    uint32_t res = isqrt(1234567890UL);
-    uint32_t end = get_mcycle();
-    printf("Result: 0x%x, Cycles: 0x%x\n", res, end - start);
-    uart_write_flush();
-
-    // using the timer
-    printf("Tick\n");
-    clint_sleep_ms(10);
-    printf("Tock\n");
+    uart_init();
+    printf("Hello World from Croc!\n");
     uart_write_flush();
     return 0;
 }
