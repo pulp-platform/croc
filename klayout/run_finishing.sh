229ab58675e943c6877525589a19cdb69e845f10
Philippe Sauter <phsauter@student.ethz.ch>
2026-08-08 18:57:00 +0200

    klayout: add seal-ring generation and GDS streamout

--- klayout/run_finishing.sh
+++ klayout/run_finishing.sh
@@ -12,6 +12,15 @@ set -e  # Exit on error
 set -u  # Error on undefined vars
 
 
+DRYRUN=0
+for arg in "$@"; do
+    [[ "$arg" == -n || "$arg" == --dry-run ]] && DRYRUN=1
+done
+
+if [[ "$DRYRUN" -eq 1 ]]; then
+    export CROC_SKIP_TECH_SETUP=1
+fi
+
 ################
 # Setup
 ################
@@ -22,18 +31,6 @@ source "../env.sh"
 ###############
 # Technology
 ###############
-
-lef_files="$(find "$PDK_DIR_LEF_TECH" -name 'sg13g2_stdcell.lef' -exec realpath {} \;) \
-     $(find "$PDK_DIR_LEF_CELLS" -name 'sg13g2_tech.lef' -exec realpath {} \;) \
-     $(find "$PDK_DIR_LEF_SRAMS" -name 'RM_IHPSG13*.lef' -exec realpath {} \;) \
-     $(find "$PDK_DIR_LEF_IOS" -name 'sg13g2_io.lef' -exec realpath {} \;) \
-     $(find "$PDK_DIR_LEF_BOND" -name '*.lef' -exec realpath {} \;)"
-
-gds_files="$(find "$PDK_DIR_GDS_CELLS" -name 'sg13g2_stdcell.gds' -exec realpath {} \;) \
-     $(find "$PDK_DIR_GDS_SRAMS" -name 'RM_IHPSG13*.gds' -exec realpath {} \;) \
-     $(find "$PDK_DIR_GDS_IOS" -name 'sg13g2_io.gds' -exec realpath {} \;) \
-     $(find "$PDK_DIR_GDS_BOND" -name '*.gds' -exec realpath {} \;)"
-
 
 SEAL_RING_SPACE=42
 
@@ -71,50 +68,126 @@ EOF
 
 run_cmd() {
     if [ "$DRYRUN" = 1 ]; then
-        echo $1
+        printf '%s\n' "$1"
     else
         eval $1
     fi
 }
 
 
-def_to_gds() {
-    run_cmd "mkdir -p out"
-    run_cmd "echo [INFO][KLayout] Running LEF/DEF to stream"
-    run_cmd "klayout -zz \
-        -rd gds_allow_empty=True \
-        -rd design_name=\"$TOP_DESIGN\" \
-        -rd in_def=\"../openroad/out/${PROJ_NAME}.def\" \
-        -rd layer_map=\"$KLAYOUT_PATH/tech/sg13g2.map\" \
-        -rd lef_files=\"$lef_files\" \
-        -rd gds_files=\"$gds_files\" \
-        -rd out_file=\"out/${PROJ_NAME}.gds.gz\" \
-        -rm scripts/def2stream.py" \
-        > out/def2stream.log
+supports_split_topmetal_fill() {
+    grep -q "no_topmetal_1" "$KLAYOUT_PATH/tech/scripts/filler.py"
 }
 
 
-gen_seal_ring() {
-    run_cmd "echo [INFO][KLayout] Creating seal ring"
-    run_cmd "die_um=($(grep DIEAREA ../openroad/out/${PROJ_NAME}.def | grep -oE '[0-9]+' | tail -n 2 | xargs))"
-    run_cmd "die_width=$(( ${die_um[-2]} / 1000 ))"
-    run_cmd "die_height=$(( ${die_um[-1]} / 1000 ))"
-    run_cmd "seal_width=$(( $die_width + 2 * $SEAL_RING_SPACE ))"
-    run_cmd "seal_height=$(( $die_width + 2 * $SEAL_RING_SPACE ))"
-    run_cmd "echo [INFO][KLayout] Read ../openroad/out/${PROJ_NAME}.def - die area: ${die_width} um x ${die_height} um"
-    run_cmd "echo [INFO][KLayout] Chip dimensions with seal: ${seal_width} um x ${seal_height} um"
-    run_cmd "klayout -n sg13g2 -zz \
-        -r $KLAYOUT_PATH/tech/scripts/sealring.py \
-        -rd width=$seal_width  \
-        -rd height=$seal_height \
+collect_def_to_gds_inputs() {
+    lef_files="$(find "$PDK_DIR_LEF_TECH" -name "$PDK_TECH_LEF_FILE" -exec realpath {} \;) \
+         $(find "$PDK_DIR_LEF_CELLS" -name "$PDK_STDCELL_LEF_FILE" -exec realpath {} \;) \
+         $(find "$PDK_DIR_LEF_SRAMS" -name 'RM_IHPSG13*.lef' -exec realpath {} \;) \
+         $(find "$PDK_DIR_LEF_IOS" -name "$PDK_IO_LEF_FILE" -exec realpath {} \;) \
+         $(find "$PDK_DIR_LEF_BOND" -name "$PDK_BONDPAD_LEF" -exec realpath {} \;)"
+
+    local required_gds_files=(
+        "$PDK_DIR_GDS_CELLS/$PDK_STDCELL_GDS_FILE"
+        "$PDK_DIR_GDS_SRAMS/RM_IHPSG13_1P_512x32_c2_bm_bist.gds"
+        "$PDK_DIR_GDS_IOS/$PDK_IO_GDS_FILE"
+        "$PDK_DIR_GDS_BOND/$PDK_BONDPAD_GDS"
+    )
+
+    gds_files="${required_gds_files[*]}"
+    [[ "$DRYRUN" -eq 1 ]] && return 0
+
+    local missing_gds=0
+    for gds_file in "${required_gds_files[@]}"; do
+        if [[ ! -f "$gds_file" ]]; then
+            missing_gds=1
+            break
+        fi
+    done
+
+    if [[ "$missing_gds" -eq 1 && "$CROC_TECHNOLOGY_VIEW" == "public mirror" ]]; then
+        echo "[INFO][KLayout] Downloading missing public PDK GDS"
+        "$CROC_ROOT/scripts/download_gds.sh"
+    fi
+
+    for gds_file in "${required_gds_files[@]}"; do
+        if [[ ! -f "$gds_file" ]]; then
+            echo "[ERROR][KLayout] Missing required GDS: $gds_file" >&2
+            exit 1
+        fi
+    done
+
+    gds_files="$(find "$PDK_DIR_GDS_CELLS" -name "$PDK_STDCELL_GDS_FILE" -exec realpath {} \;) \
+         $(find "$PDK_DIR_GDS_SRAMS" -name 'RM_IHPSG13*.gds' -exec realpath {} \;) \
+         $(find "$PDK_DIR_GDS_IOS" -name "$PDK_IO_GDS_FILE" -exec realpath {} \;) \
+         $(find "$PDK_DIR_GDS_BOND" -name "$PDK_BONDPAD_GDS" -exec realpath {} \;)"
+}
+
+
+def_to_gds() {
+    collect_def_to_gds_inputs
+    run_cmd "mkdir -p out"
+    run_cmd "echo [INFO][KLayout] Running LEF/DEF to stream"
+    run_cmd "klayout -nc -rx -zz \
+        -rd gds_allow_empty=True \
+        -rd design_name=\"$TOP_DESIGN\" \
+        -rd in_def=\"../openroad/out/${PROJ_NAME}.def\" \
+        -rd layer_map=\"$PDK_LAYER_MAP_FILE\" \
+        -rd lef_files=\"$lef_files\" \
+        -rd gds_files=\"$gds_files\" \
+        -rd out_file=\"out/${PROJ_NAME}.gds.gz\" \
+        -rm scripts/def2stream.py \
+        > out/def2stream.log"
+}
+
+
+derive_seal_dimensions() {
+    local die_um
+    read -r -a die_um < <(grep DIEAREA "../openroad/out/${PROJ_NAME}.def" | grep -oE '[0-9]+' | tail -n 2 | xargs)
+
+    die_width=$(( die_um[-2] / 1000 ))
+    die_height=$(( die_um[-1] / 1000 ))
+    seal_width=$(( die_width + 2 * SEAL_RING_SPACE ))
+    seal_height=$(( die_height + 2 * SEAL_RING_SPACE ))
+}
+
+generate_seal_ring() {
+    local width="$1"
+    local height="$2"
+
+    run_cmd "klayout -nc -rx -zz \
+        -r scripts/generate_seal_ring.py \
+        -rd width=$width  \
+        -rd height=$height \
         -rd output=out/seal_ring.gds.gz \
         > out/gen_seal.log"
+}
+
+gen_seal_ring() {
+    run_cmd "echo [INFO][KLayout] Creating seal ring"
+
+    if [[ "$DRYRUN" -eq 1 ]]; then
+        run_cmd 'die_um=($(grep DIEAREA ../openroad/out/${PROJ_NAME}.def | grep -oE "[0-9]+" | tail -n 2 | xargs))'
+        run_cmd 'die_width=$(( die_um[-2] / 1000 ))'
+        run_cmd 'die_height=$(( die_um[-1] / 1000 ))'
+        run_cmd "seal_width=\$(( die_width + 2 * SEAL_RING_SPACE ))"
+        run_cmd "seal_height=\$(( die_height + 2 * SEAL_RING_SPACE ))"
+        run_cmd 'echo [INFO][KLayout] Read ../openroad/out/${PROJ_NAME}.def - die area: ${die_width} um x ${die_height} um'
+        run_cmd 'echo [INFO][KLayout] Chip dimensions with seal: ${seal_width} um x ${seal_height} um'
+        generate_seal_ring '$seal_width' '$seal_height'
+        return
+    fi
+
+    derive_seal_dimensions
+    run_cmd "echo [INFO][KLayout] Read ../openroad/out/${PROJ_NAME}.def - die area: ${die_width} um x ${die_height} um"
+    run_cmd "echo [INFO][KLayout] Chip dimensions with seal: ${seal_width} um x ${seal_height} um"
+    generate_seal_ring "$seal_width" "$seal_height"
 }
 
 
 merge_seal_ring() {
     run_cmd "echo [INFO][KLayout] Merging seal ring"
-    run_cmd "klayout -zz \
+    run_cmd "klayout -nc -rx -zz \
         -rm scripts/merge_sealring.py \
         -rd chip_gds=out/${PROJ_NAME}.gds.gz \
         -rd seal_gds=out/seal_ring.gds.gz \
@@ -127,25 +200,36 @@ merge_seal_ring() {
 
 
 fill_metal() {
-    run_cmd "echo [INFO][KLayout] Filling M1-M5 and TM1"
-    run_cmd "klayout -n sg13g2 -zz \
+    run_cmd "echo [INFO][KLayout] Filling routing metals for $CROC_PDK"
+    if [[ "$CROC_PDK" == "sg13cmos5l" ]]; then
+        fill_flags="-rd no_activ"
+    elif supports_split_topmetal_fill; then
+        fill_flags="-rd no_topmetal_2 -rd no_activ"
+    else
+        fill_flags="-rd no_topmetal -rd no_activ"
+    fi
+    run_cmd "klayout -n $KLAYOUT_TECH -zz \
         -r $KLAYOUT_PATH/tech/scripts/filler.py \
         -rd output_file=out/${PROJ_NAME}.metfilled.gds.gz \
-        -rd no_topmetal_2 \
-        -rd no_activ \
+        $fill_flags \
         out/${PROJ_NAME}.sealed.gds.gz \
         > out/merge_seal.log"
 }
 
 
 fill_activ() {
-    run_cmd "echo [INFO][KLayout] Filling M1-M5 and TM1"
-    run_cmd "klayout -n sg13g2 -zz \
+    run_cmd "echo [INFO][KLayout] Filling active layers for $CROC_PDK"
+    if [[ "$CROC_PDK" == "sg13cmos5l" ]]; then
+        fill_flags="-rd no_metal"
+    elif supports_split_topmetal_fill; then
+        fill_flags="-rd no_topmetal_2 -rd no_topmetal_1 -rd no_metal"
+    else
+        fill_flags="-rd no_topmetal -rd no_metal"
+    fi
+    run_cmd "klayout -n $KLAYOUT_TECH -zz \
         -r $KLAYOUT_PATH/tech/scripts/filler.py \
         -rd output_file=out/${PROJ_NAME}.filled.gds.gz \
-        -rd no_topmetal_2 \
-        -rd no_topmetal_1 \
-        -rd no_metal \
+        $fill_flags \
         out/${PROJ_NAME}.metfilled.gds.gz"
 }
 
@@ -153,8 +237,6 @@ fill_activ() {
 ####################
 # Parse Arguments
 ####################
-
-DRYRUN=0
 
 # default action if no argument is given
 if [ $# -eq 0 ]; then
